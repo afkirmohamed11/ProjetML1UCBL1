5e6143c40b1107ffa53354f94174b34bcaafc83a
hamzaae <motassimhamza99@gmail.com>
2025-12-13 16:12:37 +0100

    update schema

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,12 +1,32 @@
 from pydantic import BaseModel
 
 class PredictRequest(BaseModel):
-    pass
-    # TODO: Define the input features for the prediction model
-    # feature_1: float
-    # feature_n: float
-    
+    # TODO : Define the input features for the prediction request based on the model requirements
+    gender: str
+    senior_citizen: bool
+    partner: bool
+    dependents: bool
+    tenure: int
+
+    phone_service: bool
+    multiple_lines: str
+
+    internet_service: str
+    online_security: str
+    online_backup: str
+    device_protection: str
+    tech_support: str
+    streaming_tv: str
+    streaming_movies: str
+
+    contract: str
+    paperless_billing: bool
+    payment_method: str
+
+    monthly_charges: float
+    total_charges: float
 
 
 class PredictResponse(BaseModel):
     churn_probability: float
+    churn: bool
