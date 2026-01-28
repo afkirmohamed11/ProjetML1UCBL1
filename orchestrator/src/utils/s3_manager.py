cf5172819249c4bf3db47e4c8ef04a0635802105
Admin <bentouhamimohamedrida@gmail.com>
2026-01-28 03:14:17 +0100

    fix the probleme of secrets

--- orchestrator/src/utils/s3_manager.py
+++ orchestrator/src/utils/s3_manager.py
@@ -4,10 +4,41 @@ import logging
 import pandas as pd
 from sqlalchemy import create_engine, text
 
+
+def get_secrets():
+
+    RDS_HOST = "mlprojectdb.cx4k04y48cjc.eu-north-1.rds.amazonaws.com"
+    RDS_PORT = 5432
+    RDS_USER = "postgres"
+    RDS_PASSWORD = "mlpipeline"
+    RDS_DB_NAME = "telco_churn"
+    TABLE_NAME = "secrets"
+    
+    # Création de l'URL de connexion avec psycopg2 explicite
+    connection_url = f"postgresql+psycopg2://{RDS_USER}:{RDS_PASSWORD}@{RDS_HOST}:{RDS_PORT}/{RDS_DB_NAME}"
+
+    try:
+        print("Connexion à RDS en cours...")
+        # Requête SQL simple
+        query = f"SELECT * FROM {TABLE_NAME}"
+        
+        # Passer directement l'URL de connexion à pandas (sans créer d'engine)
+        ref_df = pd.read_sql_query(query, connection_url)
+        
+        print(f"Extraction réussie : {len(ref_df)} lignes de réf")
+        ref_df.head(1).to_dict()
+        return ref_df["ACCESS_KEY"][0],ref_df["SECRET_KEY"][0]
+
+    except Exception as e:
+        print(f"Erreur lors de la connexion à RDS : {e}")
+        return None
+
+
+
 def put_model_in_s3(my_model):
     # 1. Setup your credentials
-    ACCESS_KEY = '<access_key>'
-    SECRET_KEY = '<secrect_key>'
+    
+    ACCESS_KEY ,SECRET_KEY =get_secrets()
     BUCKET_NAME = 'prodmodle'
     S3_FILE_PATH = 'production_model/champion.pkl'  # Where it will live in S3
 
@@ -64,3 +95,5 @@ def update_customer_training_status():
     except Exception as e:
         logging.error(f"UPDATE Error: {e}")
         return None
+    
+
